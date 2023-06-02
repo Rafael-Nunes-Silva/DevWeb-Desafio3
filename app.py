52b98f5fb9287f4b73723074804de63bb79a445d
Rafael Silva <76886825+Rafael-Nunes-Silva@users.noreply.github.com>
2023-06-02 07:14:08 -0300

    Arrumei a config do MYSQL_HOST

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config["MYSQL_Host"] = "127.0.0.1"
+app.config["MYSQL_HOST"] = "127.0.0.1"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "fatec"
 app.config["MYSQL_DB"] = "Unes"
